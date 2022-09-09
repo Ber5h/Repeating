9872e403d7009b82bd455572676d0bb98db0a7ab
Ber5h <102763351+Ber5h@users.noreply.github.com>
2022-09-09 10:42:39 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+class Archer
+{
+	private string name;
+	private int shots;
+	private int totalAttack;
+	private int defence;
+	private int totalLife;
+	private int life;
+	public Archer()
+    {
+		name = "Полное прекращение работы телеграмм в России";
+		shots = 10; //зачем они вообще нужны
+		totalAttack = 10;
+		defence = 8;
+		TotalLife = 20;
+		life = 20;
+    }
+	public Archer(string name, int shots, int totalAttack, int defence, int Totallife)
+	{
+		this.name = name;
+		this.shots = shots;
+		this.totalAttack = totalAttack;
+		this.defence = defence;
+		this.totalLife = Totallife;
+		this.life = Totallife;
+	}
+	int LifePersent
+    {
+		get
+        {
+			return life / totalLife * 100;
+        }
+    }
+	int Attack
+    {
+		get
+        {
+			totalAttack*LifePercent/ 100;
+        }
+    }
+	bool Alive
+    {
+		get
+        {
+			return (life > 0);
+        }
+    }
+	private void Damage (int wound)
+    {
+		life = life - wound;
+    }
+	public void Shut (Archer opponent)
+    {
+		try
+		{
+			message_shut.Text = $"{this.Name} выстрелил в {opponent.Name}";
+			opponent.Damage(this.Attack - opponent.defence);
+			this.shots--;
+		}
+		catch (this.shots <= 0)
+	    {
+			throw new Exception("У игрока не осталось стрел");
+        }
+    }
+	public string Name
+    {
+        get
+        {
+			return name;
+        }
+    }
+	public int Shots
+    {
+		get
+        {
+			return shots;
+        }
+		set
+        {
+			try
+            {
+				shots = value;
+            }
+			catch (value <= 0)
+				{
+				throw new Exception("Значение не может быть меньше нуля");
+            }
+        }
+    }
+		public int TotalAttack
+    {
+		get
+        {
+			return totalAttack;
+        }
+		set
+        {
+			try
+            {
+				totalAttack = value;
+            }
+			catch (value <= 0)
+				{
+				throw new Exception("Значение не может быть меньше нуля");
+            }
+        }
+    }
+		public int Defence
+    {
+		get
+        {
+			return defence;
+        }
+		set
+        {
+			try
+            {
+				defence = value;
+            }
+			catch (value <=0)
+				{
+				throw new Exception("Значение не может быть меньше нуля");
+            }
+        }
+    }
+		public int Totallife
+    {
+        get
+        {
+			return totalLife;
+        }
+		set
+        {
+            try
+            {
+				totalLife = value;
+            }
+			catch (value <= 0)
+				{
+				throw new Exception("Значение не может быть меньше нуля");
+            }
+        }
+    }
+		public int Life
+    {
+		get
+		{ return life; }
+    }
+
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Элементы_Управления_в_форме
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shut_message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // shut_message
+            // 
+            this.shut_message.AutoSize = true;
+            this.shut_message.Location = new System.Drawing.Point(295, 137);
+            this.shut_message.Name = "shut_message";
+            this.shut_message.Size = new System.Drawing.Size(35, 13);
+            this.shut_message.TabIndex = 0;
+            this.shut_message.Text = "label1";
+            this.shut_message.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shut_message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shut_message;
+    }
+}
+
